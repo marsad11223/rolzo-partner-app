4d4bf20b8d13a234411a164f7473b51a5e99e817
Abdullah Shafiq <iamabdullahshafiq@gmail.com>
2023-06-04 21:12:21 +0500

    Remove inline styling for warning component

--- src/screens/BookingScreen/BookingDetailScreen/Warning.jsx
+++ src/screens/BookingScreen/BookingDetailScreen/Warning.jsx
@@ -3,36 +3,36 @@ import { View, StyleSheet, Text, Image } from 'react-native';
 
 import { icons } from '../../../assets/images';
 
-const Warining = ({ title = '' }) => {
+const Warning = ({ title = '' }) => {
   return (
-    <View style={{
-      backgroundColor: 'rgba(208,2,27,.1)',
-      borderRadius: 10,
-      borderWidth: 1,
-      borderColor: '#d0021b',
-      marginBottom: 30,
-      padding: 20,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
+    <View style={styles.warningContainer}>
       <Image source={icons.warningIcon} style={styles.warningIcon} />
-      <Text style={{
-        marginLeft: 10,
-        fontSize: 16
-      }}>
-        {title}
-      </Text>
+      <Text style={styles.warningText}>{title}</Text>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
+  warningContainer: {
+    backgroundColor: 'rgba(208, 2, 27, .1)',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#d0021b',
+    marginBottom: 30,
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   warningIcon: {
     width: 18,
     height: 18,
     resizeMode: 'contain',
   },
+  warningText: {
+    marginLeft: 10,
+    fontSize: 16,
+  },
 });
 
-export default Warining;
+export default Warning;
